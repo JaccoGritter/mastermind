b86ef148c3c7bcd7d5d80ebd188462d7673c41dd
Jacco Gritter <jacco.gritter@gmail.com>
2019-11-26 15:36:34 +0100

    changed App to statefull component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import './App.css';
-import Hint from './hint';
+//import Hint from './Hint';
+import Board from './Board';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      guesses: []         
+    }
+
+    let hintArray = [];
+    for(let i=0; i<10; i++) hintArray.push(['w', 'b', 'b', 'n']);
+    this.state.hints = hintArray;
+
+  }
+
+
 
   render() {
     return (
       <div className="main">
         <p id="guessblock">Master Mind</p>
-        <div className="playingfield">
-          <div className="hints">
-            <p>hints</p>
-            <Hint hints={['w', 'w', 'w', 'n']} />
-            <Hint hints={['n', 'n', 'b', 'w']} />
-            <Hint hints={['b', 'b', 'w', 'b']} />
-          </div>
-          <div className="guesses">
-            <p>guesses</p>
-          </div>
-        </div>
-
+        <Board hints={this.state.hints}/>
       </div>
     );
   }
