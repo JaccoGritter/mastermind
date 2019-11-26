0f1e1405eb19d43d3b674faaf1c3a9766a9d9987
Jacco Gritter <jacco.gritter@gmail.com>
2019-11-26 15:35:33 +0100

    clean-up of hint.js

--- src/hint.js
+++ src/hint.js
@@ -6,18 +6,11 @@ import noPin from './images/noPin.png';
 class Hint extends React.Component {
     constructor(props) {
         super(props);
-        this.renderHints = this.renderHints.bind(this);
+        this.renderHint = this.renderHint.bind(this);
     }
 
-    renderHints(hints) {
-        //console.log('******' + hints);
+    renderHint(hints) {
         const pins = hints.map(hint => {
-            // const blackPin = '\u26ab';
-            // const whitePin = '\u26aa';
-            // const noPin = '\u25ef';
-            // const blackPin = '../public/blackPin.png';
-            // const whitePin = '../public/whitePin.png';
-            // const noPin = '../public/noPin.png';
             switch (hint) {
                 case 'b':
                     return blackPin;
@@ -34,7 +27,6 @@ class Hint extends React.Component {
             <div className="hint">
             <span><img src={pins[0]} alt="pin1" /></span> <span><img src={pins[1]} alt="pin2" /></span><br />
             <span><img src={pins[2]} alt="pin3" /></span> <span><img src={pins[3]} alt="pin4" /></span>
-            
             </div>
         );
     }    
@@ -42,7 +34,7 @@ class Hint extends React.Component {
     render() {
 
         return (
-            this.renderHints(this.props.hints)
+            this.renderHint(this.props.hints)
         );
     }
 }
