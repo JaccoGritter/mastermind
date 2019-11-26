fcc5819582ca6f210b22ae441de0359f22f3a21a
Jacco Gritter <jacco.gritter@gmail.com>
2019-11-26 16:25:43 +0100

    cleanup

--- src/Guess.js
+++ src/Guess.js
@@ -6,17 +6,16 @@ import greenGuess from './images/greenGuess.png';
 import blueGuess from './images/blueGuess.png';
 import yellowGuess from './images/yellowGuess.png';
 import noGuess from './images/noGuess.png';
-import { directive } from '@babel/types';
 
 class Guess extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className = "guess">
             <span><img src={noGuess} alt="guess1"/></span>
-            <span><img src={blueGuess} alt="guess2"/></span>
-            <span><img src={greenGuess} alt="guess3"/></span>
-            <span><img src={yellowGuess} alt="guess4"/></span>
+            <span><img src={noGuess} alt="guess2"/></span>
+            <span><img src={noGuess} alt="guess3"/></span>
+            <span><img src={noGuess} alt="guess4"/></span>
             </div>
         )
     }
