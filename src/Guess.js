36c116bfc290ccac840b254a5448f5c72dd4983c
Jacco Gritter <jacco.gritter@gmail.com>
2019-11-27 09:43:12 +0100

    added render function

--- src/Guess.js
+++ src/Guess.js
@@ -8,15 +8,47 @@ import yellowGuess from './images/yellowGuess.png';
 import noGuess from './images/noGuess.png';
 
 class Guess extends React.Component {
+    constructor(props) {
+        super(props);
+        this.renderGuesses = this.renderGuesses.bind(this);
+    }
+
+    renderGuesses(guesses) {
+            const guessesToRender = guesses.map(element => {
+                switch(element) {
+                    case 'yellow':
+                        return yellowGuess;
+                    case 'green':
+                        return greenGuess;
+                    case 'white':
+                        return whiteGuess;
+                    case 'black':
+                        return blackGuess;
+                    case 'red':
+                        return redGuess;
+                    case 'blue':
+                        return blueGuess;
+                    case 'no':
+                        return noGuess;
+                default:
+                    return false;
+                }
+        });
+
+        return (
+            <div className = "guess">
+            <span><img src={guessesToRender[0]} alt="guess1"/></span>
+            <span><img src={guessesToRender[1]} alt="guess2"/></span>
+            <span><img src={guessesToRender[2]} alt="guess3"/></span>
+            <span><img src={guessesToRender[3]} alt="guess4"/></span>
+            </div>
+        )
+
+    }
 
     render() {
         return (
-            <div className = "guess">
-            <span><img src={noGuess} alt="guess1"/></span>
-            <span><img src={noGuess} alt="guess2"/></span>
-            <span><img src={noGuess} alt="guess3"/></span>
-            <span><img src={noGuess} alt="guess4"/></span>
-            </div>
+            this.renderGuesses(this.props.guess)
         )
     }
 }
