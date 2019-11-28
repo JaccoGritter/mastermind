4b3579d8095cab4cdb13787d50018f5aee048170
Jacco Gritter <jacco.gritter@gmail.com>
2019-11-28 15:28:22 +0100

    Updates

---
+++ precache-manifest.3f389913853cf2bc8ee8276dde0e7dc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09fdc09bb67b341cc58cf3f679edf29",
+    "url": "/mastermind/index.html"
+  },
+  {
+    "revision": "6df8aa07884564ad059f",
+    "url": "/mastermind/static/css/main.506bba76.chunk.css"
+  },
+  {
+    "revision": "4a3d9c93d553226e45a4",
+    "url": "/mastermind/static/js/2.64c402c5.chunk.js"
+  },
+  {
+    "revision": "6df8aa07884564ad059f",
+    "url": "/mastermind/static/js/main.30e04b72.chunk.js"
+  },
+  {
+    "revision": "8064995eaebbad09c032",
+    "url": "/mastermind/static/js/runtime-main.186fced9.js"
+  },
+  {
+    "revision": "347f334f68b0d70616f02a63aeede36b",
+    "url": "/mastermind/static/media/woods-background-1.347f334f.jpg"
+  }
+]);
